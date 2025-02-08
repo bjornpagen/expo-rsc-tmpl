caf2b36389df34ac1571fa2ba346078ddafc8b6d
Bjorn Pagen <11238136+bjornpagen@users.noreply.github.com>
2025-02-08 12:42:29 -0600

    remove url hardcode

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -5,7 +5,6 @@ import { expoClient } from "@better-auth/expo/client"
 import * as SecureStore from "expo-secure-store"
 
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:8081",
 	plugins: [
 		expoClient({
 			scheme: "myapp",
